c62f440293edd5e18ece1babfbf3dedbf451976e
Shafay Abid <shafayabid4@gmail.com>
2024-01-18 19:59:00 +0500

    solve: excercise 1 to 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+
+    var tempArr = arr;
+
+    
+    for(let i = 0; i < nums.length; i++){
+        while(tempArr.includes(nums[i])){
+            let index = tempArr.indexOf(nums[i])
+            tempArr.splice(index, 1);
+        }
+    }
+
+    return tempArr;
 
 };
 
