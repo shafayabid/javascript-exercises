8082d4801ffe99355aa914d4334f8e47ed552426
Shafay Abid <shafayabid4@gmail.com>
2024-01-28 12:54:32 +0500

    solve: 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,31 @@
-const sumAll = function() {
+const sumAll = function(numStart, numEnd) {
+    
+    let start;
+    let end;    
+    
+    if(numStart < numEnd){
+
+        start = numStart;
+        end = numEnd;
+        
+    }else{
+
+        start = numEnd;
+        end = numStart;
+
+    }
+    let sum = 0;
+
+    while(start <= end){
+        sum = sum + start;
+        start = start + 1;
+    }
+
+    if(numStart < 0 || numEnd < 0 || typeof(numStart) != "number" || typeof(numEnd) != "number"){
+        return "ERROR";
+    }else{
+        return sum;
+    }
 
 };
 
