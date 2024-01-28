ba8f00713ec5dbee9137d6ca72cc66e9c73ede35
Shafay Abid <shafayabid4@gmail.com>
2024-01-28 17:34:54 +0500

    solve: 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+
+  let ans = (fahrenheit -32) / 1.8
+
+  return Math.round(ans * 10) / 10
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+
+  let ans = (celcius * (9/5)) +32
+
+  return Math.round(ans * 10) / 10
+
 };
 
 // Do not edit below this line
